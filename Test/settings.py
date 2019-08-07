e441b71987f5cc4dca6632aaedfa6efea59dd6e3
Shreya Singh <shreyasingh7491@gmail.com>
2019-08-08 00:26:39 +0530

    done

--- Test/settings.py
+++ Test/settings.py
@@ -125,6 +125,10 @@ STATIC_URL = '/static/'
 
 
 CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:4200',
+)
+
 # CORS_ORIGIN_ALLOW_ALL = False
 #
 # CORS_ORIGIN_WHITELIST = (
